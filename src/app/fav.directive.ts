36ed1675930a84afc596ba824a52daba7dd0cbc5
Darshan S R <darshansr.niit@gmail.com>
2020-03-06 20:24:48 +0100

    custom pipe

--- src/app/fav.directive.ts
+++ src/app/fav.directive.ts
@@ -1,4 +1,4 @@
-import { Directive,HostBinding, Input} from "@angular/core"
+import { Directive,HostBinding,HostListener, Input} from "@angular/core"
 
 @Directive({
     selector:'[mwFav]',
@@ -8,7 +8,18 @@ import { Directive,HostBinding, Input} from "@angular/core"
 export class FavDirective {
     //set the class value is-favorite into the custom directive tag setting inside the mwFav setter methods from mediaItem value from the json
     @HostBinding('class.is-favorite') isFavorite =true;
+    @HostBinding('class.is-favorite-hovering') hovering =false;
+
+    @HostListener('mouseenter') onMouseEnter(){
+        this.hovering=true;
+    }
+
+    @HostListener('mouseleave') onMouseLeave(){
+        this.hovering=false;
+    }
+
     @Input() set mwFav(value){
         this.isFavorite=value
     }
+
 }
