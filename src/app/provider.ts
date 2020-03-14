78cb881eca4a3d4d697e9d134cb5b6c5f6f8b9db
Darshan S R <darshansr.niit@gmail.com>
2020-03-14 15:00:41 +0100

    mock api

---
+++ src/app/provider.ts
@@ -0,0 +1,11 @@
+import {
+    InjectionToken
+} from '@angular/core'
+
+export const lookupListToken =new InjectionToken('lookupListToken')
+
+export const lookupLists = {
+    mediums:['Movies','Series']
+}
+
+ 
